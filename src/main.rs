5b68761335d22411cbfdd23b52ea49f8707f16e7
Narendra <naskastark11@gmail.com>
2025-11-16 19:11:40 +0530

    Client handshake is a thing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use tokio::net::{TcpListener, TcpStream};
 
 use tokio::io::BufReader;
 
+use crate::resp_frame::RespFrame;
+
 mod parser;
 mod resp_frame;
 pub mod serializer;
@@ -14,7 +16,7 @@ async fn main() {
     println!("Echo server listening on {}", bind_addr);
 
     loop {
-        let (mut stream, _) = listener.accept().await.unwrap();
+        let (stream, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
             handle_connection(stream).await;
         });
@@ -37,17 +39,63 @@ async fn handle_connection(stream: TcpStream) {
             Ok(frame) => {
                 println!("Received : {:?}", frame);
 
-                if let Err(e) = serializer::serialize_frame(&mut write_half, frame).await {
-                    println!("Error writing toclient : {}", e);
+                // Process the command and get response
+                let response = handle_command(frame);
+
+                if let Err(e) = serializer::serialize_frame(&mut write_half, response).await {
+                    println!("Error writing to client : {}", e);
                     break;
                 }
             }
 
             Err(e) => {
-                println!("Error parsing frame or connectiong closed: {}", e);
+                println!("Error parsing frame or connection closed: {}", e);
                 break;
             }
         }
     }
     println!("Client disconnected: {:?}", write_half.peer_addr());
+}
+
+// https://redis.io/docs/latest/develop/reference/protocol-spec/#client-handshake
+fn handle_command(frame: RespFrame) -> RespFrame {
+    let RespFrame::Array(args) = frame else {
+        return RespFrame::Error("ERR command must be an array".to_string());
+    };
+
+    if args.is_empty() {
+        return RespFrame::Error("ERR empty command".to_string());
+    }
+
+    // extract command name
+    let command_name = match &args[0] {
+        RespFrame::BulkString(bytes) => String::from_utf8_lossy(bytes).to_uppercase(),
+        RespFrame::SimpleString(s) => s.to_uppercase(),
+        _ => return RespFrame::Error("ERR invalid command format".to_string()),
+    };
+
+    match command_name.as_str() {
+        "PING" => {
+            if args.len() == 1 {
+                RespFrame::SimpleString("PONG".to_string())
+            } else if args.len() == 2 {
+                // Echo back the argument
+                args[1].clone()
+            } else {
+                RespFrame::Error("ERR wrong number of arguments for 'ping' command".to_string())
+            }
+        }
+        "ECHO" => {
+            if args.len() == 2 {
+                args[1].clone()
+            } else {
+                RespFrame::Error("ERR wrong number of arguments for 'echo' command".to_string())
+            }
+        }
+        "COMMAND" => {
+            //  minimal response to prevent the assertion failure
+            RespFrame::Array(vec![])
+        }
+        _ => RespFrame::Error(format!("ERR unknown command '{}'", command_name)),
+    }
 }
